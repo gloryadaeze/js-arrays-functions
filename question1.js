d7e3c82193fb0aac41b3285f33d78322d1857c45
gloryadaeze <daeze7@gmail.com>
2023-12-14 10:08:45 +0100

    Week9 final assignment

---
+++ question1.js
@@ -0,0 +1,7 @@
+// Sample array
+const myArray = [10, 20, 30, 40, 50];
+
+// Loop to print numbers from 0 to the length of the array
+for (let i = 0; i < myArray.length; i++) {
+    console.log(i);
+}
---
+++ question2.js
@@ -0,0 +1,9 @@
+function printArrayValues(arr) {
+    for (let i = 0; i < arr.length; i++) {
+        console.log(arr[i]);
+    }
+}
+
+// Example usage with an array
+const myArray = [10, 20, 30, 40, 50];
+printArrayValues(myArray);
